97aa3a2fea78186ff04cdc0827526cc5f952de6f
Justin-Guimond <trustinjusting@gmail.com>
2023-07-07 14:36:30 -0400

    Commit 1 - added home route

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Showing
 
 # Create your views here.
+def home(request):
+    showings = Showing.objects.all()
+    return render(request, 'home.html'), {
+        'showings' : showings
+    }
