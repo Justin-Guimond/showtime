4ea53f1269ce403c1baaacf7e4310d4aaeb23ffb
Justin-Guimond <trustinjusting@gmail.com>
2023-07-10 09:51:57 -0400

    Commit 5 - created index.html route for showing list

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('showings/', views.showings_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+from django.forms.models import BaseModelForm
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Showing
@@ -5,3 +6,10 @@ from .models import Showing
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def showings_index(request):
+    showing = Showing.objects.all()
+    return render(request, 'showings/index.html', {
+        'showing' : showing
+    })
+    
